cb4f55ef471263cde58df56b1db36b543d30cad5
tamtale <tamtale@gmail.com>
2019-11-11 20:59:07 -0600

    working on functor proofs

--- monads.v
+++ monads.v
@@ -114,11 +114,11 @@ mkMonad
   {
     M: Functor;
     Eta(A: Type): A -> FObj M A;
-    Mu(A: Type): FObj M (FObj M A) -> FObj M A;
+    Mu: forall {A: Type}, FObj M (FObj M A) -> FObj M A;
     MuNatural : forall {A: Type}, 
-      (M.(FMorph)(Mu A)) ;; (Mu A) ~~ Mu (FObj M A) ;; (Mu A);
-    EtaNatural1 : forall {A: Type}, Eta(FObj M A) ;; (Mu A) ~~ @id(FObj M A);
-    EtaNatural2 : forall {A: Type}, FMorph M (Eta A) ;; (Mu A) ~~ @id(FObj M A)
+      FMorph M (@Mu A) ;; Mu ~~ Mu ;; Mu;
+    EtaNatural1 : forall {A: Type}, Eta(FObj M A) ;; Mu ~~ @id(FObj M A);
+    EtaNatural2 : forall {A: Type}, FMorph M (Eta A) ;; (Mu) ~~ @id(FObj M A)
   }.
 
 Fixpoint concat {A: Type} (xs ys: list A): list A := 
@@ -137,7 +137,7 @@ induction xs as [|first rest IH].
 + simpl.
   rewrite -> IH.
   reflexivity.
-  Qed.
+Qed.
 
 Lemma nil_left_concat_ident :
   forall {A: Type} (xs: list A),
@@ -239,7 +239,7 @@ Record KleisliTriple: Type :=
 mkTriple {
   T: Type -> Type;
   Unit(A: Type): A -> T(A);
-  Bind: forall {A B: Type} (f: A -> T(B)), T(A) -> T(B);
+  Bind: forall {A B: Type}(f: A -> T(B)), T(A) -> T(B);
   UnitReturn(A: Type): (Bind (Unit A)) ~~ @id(T A);
   UnitComposeReturn(A B: Type): forall (f: A -> T(B)), 
     Unit A ;; Bind f ~~ f;
@@ -247,4 +247,61 @@ mkTriple {
     Bind (f ;; (Bind g)) ~~ (Bind f) ;; (Bind g)
 }.
 
-Def
+Definition fmorph_from_triple (kt: KleisliTriple) {A B: Type} (f: A -> B): (T kt A -> T kt B) :=
+(Bind kt) (f ;; @Unit kt B).
+
+
+Lemma fid_holds(kt: KleisliTriple): forall {A: Type}, @fmorph_from_triple kt A A id ~~ id.
+Proof. 
+intros. 
+unfold fmorph_from_triple. 
+unfold equiv.
+intros a.
+rewrite id_step.
+cut (Bind kt (id ;; Unit kt A) ~~ Bind kt (Unit kt A)).
+- unfold equiv.
+  intros.
+  rewrite -> H.
+  rewrite -> UnitReturn.
+  reflexivity.
+- unfold equiv.
+  intros.
+  repeat rewrite -> UnitReturn.
+  reflexivity.
+Qed.
+
+Lemma fcompose_holds(kt: KleisliTriple): forall (A B C: Type) (f: A -> B) (g: B -> C),
+  fmorph_from_triple kt (f ;;g) ~~ fmorph_from_triple kt f ;; fmorph_from_triple kt g.
+Proof. 
+intros. 
+unfold fmorph_from_triple. 
+unfold equiv.
+intros.
+rewrite -> compose_step.
+
+Definition mu_from_triple (kt: KleisliTriple) {A: Type}: (T kt (T kt A)) -> T kt A :=
+Bind kt (@id (T kt A)).
+
+Definition triple_to_monad (kt: KleisliTriple): Monad = 
+mkMonad
+  (mkFunctor 
+    (T kt)
+    (@fmorph_from_triple kt)
+    (@fid_holds kt)
+    (@fcompose_holds kt)
+  )
+  (Unit kt)
+  (@mu_from_triple kt)
+
+ .
+
+
+Definition bind_from_monad {A B: Type} (m: Monad) (f: A -> (FObj (M m) B)): (FObj (M m) A) -> (FObj (M m) B) :=
+FMorph (M m) f ;; Mu m.
+
+Definition monad_to_triple (m: Monad): KleisliTriple :=
+mkTriple 
+  (FObj (M m)) 
+  (Eta m)
+  (bind_from_monad m)
+.
